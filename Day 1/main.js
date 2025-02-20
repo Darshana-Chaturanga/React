5a8801594ac80f20bb39db84987125ba41e737f8
Darshana-Chaturanga <mpdarshanachaturanga@gmail.com>
2025-02-20 20:47:24 +0530

    My first commit

---
+++ Day 1/main.js
@@ -0,0 +1,39 @@
+
+console.log("DOM is ready");
+
+// variable declaring with var
+var message1;
+//let message2;
+
+{
+     {
+          var message2 = "Hello let{}";
+          console.log(message2);
+     }
+}
+
+message1 = "Hello world..1";
+//message2 = "Hello world..2";
+
+console.log(message1);
+console.log(message2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
